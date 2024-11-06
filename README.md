cc6c3fb4f23b6663202cfa993b8a1026bd13ce0b
lsw3014 <yus4149@gmail.com>
2024-11-07 03:35:11 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Assignment
+# LmsAutomaticCaptionGeneration
+
+<p>lms 강의 자막 생성 및 요약 프로그램</p>
+<br>
+
+## 프로젝트 개요
+
+lms 강의에 자막을 생성하고, 내용을 요약해주는 프로그램
+
+### 주요 기능
+
+1. 자동 자막 생성
+2. 전체 자막 파일 제공
+3. 강의 내용 요약
+<br>
+
+## 사용 방법
+
+ㅁㅁㅁㅁ
+
+- **본 프로그램을 사용하기 위해선 [Virtual Audio Cable(VAC)](https://vac.muzychenko.net/en/)의 설치가 필요합니다**
+<br>
+
+## 개발 환경
+
+- Window
+- Visual Studio Code
+
+|사용 오픈소스 / 라이브러리|용도|
+|--|--|
+|KoBart|텍스트 요약|
+|테스트1|테스트2|
+|테스트1|테스트2|
+<br>
+
+## 개발 일정
+
+- 추가 예정
---
+++ src/test.py
@@ -0,0 +1,20 @@
+# pyaudio test
+import pyaudio
+
+# pyaudio 객체 생성
+p = pyaudio.PyAudio()
+
+def select_microphone(index):
+   # 기기 정보 불러오기
+   device_info = p.get_device_info_by_index(index)
+   
+   # 입력 장치인지 확인
+   if device_info.get('maxInputChannels') > 0:
+      print(f"Selected Microphone: {device_info.get('name')}")
+   else:
+      print(f"No microphone at index {index}")
+
+selected_microphone = int(input("입력 장치의 번호를 입력해주세요: "))
+
+# 인덱스로 특정 입력 장치 선택
+select_microphone(selected_microphone)
